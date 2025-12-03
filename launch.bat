ffb4e1f882e1228eb677c0cd08903b759caada93
Collen Jnr <Grimlulu2@gmail.com>
2025-12-03 17:38:49 +0200

    Update launch.bat with new content

--- launch.bat
+++ launch.bat
@@ -1,7 +1,1 @@
-@echo off
-
-REM Start the backend server on port 5005
-start "Backend" cmd /k "cd backend && .\venv\Scripts\activate && python app.py --port=5005"
-
-REM Start the frontend server on port 3001 (Node/Vite/React)
-start "Frontend" cmd /k "cd frontend && npm run dev -- --port 3001"
+call ".\Bin Tip Tons\launch.bat"
